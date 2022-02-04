f4318bfbbcc732bab7905d277fe8e7899335ba51
voynow <voynow99@gmail.com>
2022-02-04 15:47:42 -0500

    Change docstring location

--- two_sum.py
+++ two_sum.py
@@ -1,14 +1,10 @@
 class Solution(object):
-    """
-    O(n^2) quadratic runtime
-    Runtime: 4019 ms, faster than 20.39% of Python online submissions for Two Sum.
-    Memory Usage: 14.2 MB, less than 89.73% of Python online submissions for Two Sum.
-    """
+
     def twoSum(self, nums, target):
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
+        O(n^2) quadratic runtime
+        Runtime: 4019 ms, faster than 20.39% of Python online submissions for Two Sum.
+        Memory Usage: 14.2 MB, less than 89.73% of Python online submissions for Two Sum.
         """
         
         for i in range(len(nums)):
@@ -16,16 +12,11 @@ class Solution(object):
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
-    """
-    O(n) linear runtime
-    Runtime: 55 ms, faster than 74.38% of Python online submissions for Two Sum.
-    Memory Usage: 14.3 MB, less than 48.40% of Python online submissions for Two Sum.
-    """
     def twoSum(self, nums, target):
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
+        O(n) linear runtime
+        Runtime: 55 ms, faster than 74.38% of Python online submissions for Two Sum.
+        Memory Usage: 14.3 MB, less than 48.40% of Python online submissions for Two Sum.
         """
 
         lookup = {}
