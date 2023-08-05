2616685862209dee8b6810baebaad29a472f14c3
voynow <voynow99@gmail.com>
2023-08-05 09:04:00 -0400

    Recursion solution

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -3,7 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
+
+class SolutionIterative(object):
     def reverseList(self, head):
         """
         :type head: ListNode
@@ -22,5 +23,25 @@ class Solution(object):
             p2 = p2_next_temp
         
         return p1
+
+class SolutionRecursion(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+    
+        def recursion(p1, p2):
+            if p2:
+                p2_next = p2.next
+                p2.next = p1
+
+                p1 = p2
+                p2 = p2_next
+
+                return recursion(p1, p2)
+            return p1
+
+        return recursion(None, head)
 
 
