3adbb2cc935e6a49ce835dda53e0bf1662a0a598
voynow <voynow99@gmail.com>
2023-08-15 07:59:16 -0400

    Trying some funky stuff with plotly

--- scripts/cluster_plot.py
+++ scripts/cluster_plot.py
@@ -3,40 +3,38 @@ import pickle
 from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+import plotly.express as px
+import pandas as pd
+
 
 def load_embeddings(filepath):
-    return pickle.load(open(filepath, 'rb'))
+    return pickle.load(open(filepath, "rb"))
+
 
 def perform_clustering(embeddings, n_clusters=5):
     kmeans = KMeans(n_clusters=n_clusters)
     return kmeans.fit_predict(embeddings)
 
+
 def reduce_dimensionality(embeddings):
     tsne = TSNE(n_components=2, random_state=42)
     return tsne.fit_transform(embeddings)
 
+
 def plot_clusters(reduced_embeddings, labels, filenames, n_clusters):
-    plt.style.use('dark_background')
-    plt.figure(figsize=(15, 10))
+    df = pd.DataFrame(reduced_embeddings, columns=['Dimension 1', 'Dimension 2'])
+    df['Cluster'] = labels
 
-    # Scatter plot for each cluster
-    for cluster_id in range(n_clusters):
-        cluster_embeddings = reduced_embeddings[labels == cluster_id]
-        plt.scatter(cluster_embeddings[:, 0], cluster_embeddings[:, 1], label=f'Cluster {cluster_id}')
+    fig = px.scatter(df, x='Dimension 1', y='Dimension 2', color='Cluster',
+                     title='Clusters of Python Files in Embedding Space',
+                     template='plotly_dark')
 
-    # Annotate the plot with filenames
-    for i, filename in enumerate(filenames):
-        plt.annotate(filename, (reduced_embeddings[i, 0], reduced_embeddings[i, 1]), fontsize=8, alpha=0.7)
+    fig.show()
 
-    plt.title('Clusters of Python Files in Embedding Space')
-    plt.xlabel('Dimension 1')
-    plt.ylabel('Dimension 2')
-    plt.legend()
-    plt.savefig('data/clusters.png')
 
 if __name__ == "__main__":
-    embeddings_dict = load_embeddings('data/embeddings.pkl')
-    embedding_list = [v['embedding'] for v in embeddings_dict.values()]
+    embeddings_dict = load_embeddings("data/embeddings.pkl")
+    embedding_list = [v["embedding"] for v in embeddings_dict.values()]
     file_names = list(embeddings_dict.keys())
     embedding_matrix = np.array(embedding_list)
 
