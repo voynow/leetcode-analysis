2f53ab67c8d45d0c9e925aeaf71e97406c35706f
voynow <voynow99@gmail.com>
2023-08-15 07:40:36 -0400

    Refactor

--- scripts/cluster_plot.py
+++ scripts/cluster_plot.py
@@ -4,39 +4,42 @@ from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
+def load_embeddings(filepath):
+    return pickle.load(open(filepath, 'rb'))
 
-embeddings = pickle.load(open('data/embeddings.pkl', 'rb'))
-embedding_list = [v['embedding'] for v in embeddings.values()]
-file_names = list(embeddings.keys())
+def perform_clustering(embeddings, n_clusters=5):
+    kmeans = KMeans(n_clusters=n_clusters)
+    return kmeans.fit_predict(embeddings)
 
-# Convert to a numpy array for clustering
-embedding_matrix = np.array(embedding_list)
+def reduce_dimensionality(embeddings):
+    tsne = TSNE(n_components=2, random_state=42)
+    return tsne.fit_transform(embeddings)
 
-# Perform K-Means clustering
-n_clusters = 5
-kmeans = KMeans(n_clusters=n_clusters)
-labels = kmeans.fit_predict(embedding_matrix)
+def plot_clusters(reduced_embeddings, labels, filenames, n_clusters):
+    plt.style.use('dark_background')
+    plt.figure(figsize=(15, 10))
 
-# Reduce dimensionality using t-SNE
-tsne = TSNE(n_components=2, random_state=42)
-reduced_embeddings = tsne.fit_transform(embedding_matrix)
+    # Scatter plot for each cluster
+    for cluster_id in range(n_clusters):
+        cluster_embeddings = reduced_embeddings[labels == cluster_id]
+        plt.scatter(cluster_embeddings[:, 0], cluster_embeddings[:, 1], label=f'Cluster {cluster_id}')
 
-# Plot the clusters with dark theme
-plt.style.use('dark_background')
-plt.figure(figsize=(15, 10))
+    # Annotate the plot with filenames
+    for i, filename in enumerate(filenames):
+        plt.annotate(filename, (reduced_embeddings[i, 0], reduced_embeddings[i, 1]), fontsize=8, alpha=0.7)
 
-# Scatter plot for each cluster
-for cluster_id in range(n_clusters):
-    cluster_embeddings = reduced_embeddings[labels == cluster_id]
-    plt.scatter(cluster_embeddings[:, 0], cluster_embeddings[:, 1], label=f'Cluster {cluster_id}')
+    plt.title('Clusters of Python Files in Embedding Space')
+    plt.xlabel('Dimension 1')
+    plt.ylabel('Dimension 2')
+    plt.legend()
+    plt.savefig('data/clusters.png')
 
-# Annotate the plot with filenames
-for i, filename in enumerate(file_names):
-    plt.annotate(filename, (reduced_embeddings[i, 0], reduced_embeddings[i, 1]), fontsize=8, alpha=0.7)
+if __name__ == "__main__":
+    embeddings_dict = load_embeddings('data/embeddings.pkl')
+    embedding_list = [v['embedding'] for v in embeddings_dict.values()]
+    file_names = list(embeddings_dict.keys())
+    embedding_matrix = np.array(embedding_list)
 
-plt.title('Clusters of Python Files in Embedding Space')
-plt.xlabel('Dimension 1')
-plt.ylabel('Dimension 2')
-plt.legend()
-plt.savefig('data/clusters.png')
-
+    labels = perform_clustering(embedding_matrix)
+    reduced_embeddings = reduce_dimensionality(embedding_matrix)
+    plot_clusters(reduced_embeddings, labels, file_names, n_clusters=5)
